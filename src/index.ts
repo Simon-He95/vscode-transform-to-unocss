adabd4ec47df5a28a9acfc054824f6320d2b290c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-20 20:05:58 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import * as vscode from 'vscode'
+
+export function activate(context: any) {
+  // todo: 解决iframe与vscode通信问题
+  vscode.commands.registerCommand('vscode-transformToUnocss.openUrl', () => {
+    const panel = vscode.window.createWebviewPanel(
+      'webview panel',
+      'Transform To Unocss',
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'media')],
+
+      },
+    )
+    const scriptPathOnDisk = vscode.Uri.joinPath(context.extensionUri, 'media', 'main.js')
+
+    // And the uri we use to load this script in the webview
+    const scriptUri = panel.webview.asWebviewUri(scriptPathOnDisk)
+    const nonce = getNonce()
+    const transformToUnocssUrl = 'https://to-unocss.netlify.app/'
+    // dev url
+    // const transformToUnocssUrl = 'http://127.0.0.1:3333/'
+    panel.webview.html = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Transform To Unocss</title>
+          <style>
+            body{
+              width:100%;
+              height:100vh;
+            }
+          </style>
+        </head>
+        <body>
+          <iframe id="transformToUnocss" src="${transformToUnocssUrl}" width="100%" height="100%"></iframe>
+          <script nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
+      </html>
+    `
+    panel.webview.onDidReceiveMessage(
+      ({ type, data }) => {
+        // 设置剪贴板内容
+        if (type === 'copy') {
+          vscode.env.clipboard.writeText(data).then(() => {
+            vscode.window.showInformationMessage('复制成功!  ✅')
+          })
+        }
+      },
+    )
+  })
+}
+
+export function deactivate() {
+}
+
+function getNonce() {
+  let text = ''
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < 32; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+
+  return text
+}
